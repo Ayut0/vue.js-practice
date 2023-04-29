c93d0afb0bd01690766bd1d747833db284eb3f77
Yuto Yamakita <yuvto.0420@gmail.com>
2023-04-29 10:20:16 -0700

    manipulate array and object

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,28 @@ const upperCase = () => {
   msg = msg.toUpperCase();
 };
 upperCase();
-const link = 'https://google.com';
+const link = "https://google.com";
 
 const isActive = true;
 const isBlack = true;
 
 // const error = 'error occurred';
-const stock = 0
+const stock = 0;
+
+const languages = ["TypeScript", "JavaScript", "Vue.js"];
+
+const users = [
+  { id: 1, name: "John", email: "john@test.com", admin: true },
+  { id: 2, name: "Mike", email: "mike@test.com", admin: false },
+  { id: 3, name: "Kevin", email: "kevin@test.com", admin: false },
+];
+
+const user = {
+  id: 1,
+  name: "James",
+  email: "james@test,com",
+  admin: true,
+};
 </script>
 
 <template>
@@ -22,7 +37,9 @@ const stock = 0
     <!-- Shorthand ver of v-bind -->
     <a :href="link">Google</a>
   </div>
-  <p class="underline" :class = "{active:isActive, back: isBlack}">How to set up v-bind</p>
+  <p class="underline" :class="{ active: isActive, back: isBlack }">
+    How to set up v-bind
+  </p>
 
   <!-- v-if condition -->
   <div>
@@ -30,6 +47,42 @@ const stock = 0
     <p v-if="stock > 5">We have a couple of stocks</p>
     <p v-else-if="stock === 0">Unfortunately, we are out of those</p>
     <p v-else>There is no error</p>
+  </div>
+
+  <!-- array -->
+  <h2>Array</h2>
+  <p>{{ languages[0] }}</p>
+  <p>{{ languages[1] }}</p>
+  <p>{{ languages[2] }}</p>
+  <!-- v-for loop -->
+  <div>
+    <ul>
+      <li v-for="(language, index) in languages" :key="index">
+        {{ language }}
+      </li>
+    </ul>
+  </div>
+
+  <!-- object -->
+  <h2>Object</h2>
+  <p v-for="user in users" :key="user">
+    {{ user.id }}:{{ user.name }}:{{ user.email }}
+  </p>
+  <h3>Unordered list</h3>
+  <ul>
+    <li v-for="user in users" :key="user.id">
+      {{ user.id }}:{{ user.name }}:{{ user.email }}
+    </li>
+  </ul>
+  <h3>Properties in order</h3>
+  <div v-for="(value, name) in user" :key="value">{{ name }}:{{ value }}</div>
+
+  <!-- To show the index of the array, use the second argument of the v-for -->
+  <h3>v-for:array and V-for:object</h3>
+  <div v-for="user in users" :key="user.id">
+    <div v-for="(value, name) in user" :key="value">
+      {{ name }}:{{ value }}
+    </div>
   </div>
 </template>
 
